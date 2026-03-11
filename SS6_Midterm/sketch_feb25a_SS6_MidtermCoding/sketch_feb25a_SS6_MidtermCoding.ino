fc8593e60dd15eb32bb0da708bd0c0e8db524340
PatrickMannix <94652969+PatrickMannix@users.noreply.github.com>
2026-03-10 23:25:07 -0400

    Add files via upload

---
+++ SS6_Midterm/sketch_feb25a_SS6_MidtermCoding/sketch_feb25a_SS6_MidtermCoding.ino
@@ -0,0 +1,74 @@
+const pins = [1, 2, 3, 4, 5, 6, 7]; 
+
+const potPin = pins[0];      
+const ldrPin = pins[1];      
+const touchPin = pins[2];    
+const btnPin = pins[3];      
+const pwmLedPin = pins[4];   
+const digitalLedPin = pins[5]; 
+const servoPin = pins[6];    
+const piezoPin = 8;          
+
+let btnState = 0;
+let lastBtnState = 1;
+let systemActive = false;
+let lastUpdate = 0;
+let interval = 1000;
+let brightnessArray = [0, 85, 170, 255]; 
+
+function setup() {
+  pinMode(btnPin, "INPUT_PULLUP");
+  pinMode(digitalLedPin, "OUTPUT");
+  pinMode(pwmLedPin, "OUTPUT");
+  pinMode(servoPin, "SERVO");
+  pinMode(piezoPin, "OUTPUT");
+  
+  
+  analogReadResolution(12); 
+}
+
+function loop() {
+  let now = millis();
+  
+  
+  let potVal = analogRead(potPin);     
+  let ldrVal = analogRead(ldrPin);     
+  let touchVal = analogRead(touchPin); 
+  btnState = digitalRead(btnPin);
+
+  
+  if (btnState === 0 && lastBtnState === 1) {
+    systemActive = !systemActive;
+  }
+  lastBtnState = btnState;
+
+  
+  if (systemActive && (touchVal > 1500 || potVal > 500)) {
+    digitalWrite(digitalLedPin, 1);
+
+    
+    let angle = Math.floor((potVal / 4095) * 180);
+    servoWrite(servoPin, angle);
+
+    
+    let invertedLDR = 4095 - ldrVal; 
+    for (let i = 0; i < brightnessArray.length; i++) {
+      if (invertedLDR > (i * 1000)) {
+        analogWrite(pwmLedPin, brightnessArray[i]);
+      }
+    }
+
+    
+    if (now - lastUpdate >= interval) {
+      lastUpdate = now;
+      for (let j = 0; j < 2; j++) {
+        tone(piezoPin, 880, 50); 
+      }
+    }
+  } else {
+    
+    digitalWrite(digitalLedPin, 0);
+    analogWrite(pwmLedPin, 0);
+    servoWrite(servoPin, 90); 
+  }
+}
